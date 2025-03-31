b69e7b580092cc606431c58ca3f8c57e0758ad73
ong456 <cosmos050406@gmail.com>
2025-03-31 13:22:14 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+let navbar;
+let adbanner;
+let product;
+let gnb;
+let icon;
+let underg;
+let textbox;
+
+let state=0;
+
+function preload(){
+  navbar=loadImage('01_NavBar.png');
+  adbanner=loadImage('02_AdBanner.png');
+  product=loadImage('03_Product.png');
+  gnb=loadImage('04_GNB.png');
+  icon=loadImage('05_ai_icon.png');
+  underg=loadImage('06_green.png');
+  textbox=loadImage('07_textbox.png');
+}
+
+function setup() {
+  createCanvas(393, 852);
+}
+
+function draw() {
+  background(225);
+  if(state==0){
+    image(navbar, 0,0,393,128);
+    image(adbanner, 0,128,393,284);
+    image(product, 0,412,393,440);
+    image(gnb, 0,764,393,88);
+    image(icon, 320,688,60,60);
+  }
+  else if(state==1){
+    image(navbar, 0,0,393,128);
+    image(adbanner, 0,128,393,284);
+    image(product, 0,412,393,440);
+    image(underg, 0,612,393,152);
+    image(gnb, 0,764,393,88);
+    image(icon, 320,688,60,60);
+  }
+}
+
+function mouseClicked(){
+  if(mouseX>=320&&mouseX<=380){
+    if(mouseY>=688&&mouseY<=748){
+      if(state==0){
+        state=1;
+      }
+      else if(state==1){
+        state=0;
+      }
+    }
+  }
+}
